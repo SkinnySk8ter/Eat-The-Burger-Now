a7d730123f5e7b2830cca8dbb4b830479cadea4f
Jody Jones <jodybjones@yahoo.com>
2020-03-31 14:35:05 -0400

    should be able to either get data from DB...insert a burger into DB...or update a burger in the db...

--- controller/burger_controller.js
+++ controller/burger_controller.js
@@ -5,15 +5,34 @@ var router = express.Router();
 // Import the model (burgers.js) to use its database functions.
 var cat = require("../models/burgers.js");
 
-router.get("/", function(req, res) {
-    burger.all(function(data) {
-      var hbsObject = {
-        burger: data
+router.get("/", (req, res) => {
+  burgers.selectAll((data) => {
+      const burgerObject = {
+          burgers: data
       };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+      res.render("index", burgerObject);
+  })
+});
+
+router.post("/api/burger", (req, res) => {
+  burger.insertOne([req.body.burgername], (result) => {
+      // Send back the ID
+      res.json({ id: result.insertId });
   });
+});
+
+router.put("/api/burger/:id", (req, res) => {
+  let condition = "id = " + req.params.id;
+  burger.updateOne(condition, (result) => {
+      if (result.affectedRows == 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+      } else {
+          res.status(200).end();
+      }
+  });
+});
+  
 
   // Export routes for server.js to use.
 module.exports = router;
